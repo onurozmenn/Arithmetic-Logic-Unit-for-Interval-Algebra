ab874ae18f6cf2d33eb28fccc3a9d8a586a45f75
Onur Özmen <onurozmen2001@gmail.com>
2025-03-25 00:44:38 +0300

    div fixed.

--- IALU.v
+++ IALU.v
@@ -532,8 +532,8 @@ module Four_Bit_Substractor_Div(input [`EXP-1:0] A,B,
 		FullAdder fad0 (.a(A[0]), .b(~B[0]), .cin(1'b1), .sum(t_sum[0]), .cout(carry[0]));
 		FullAdder fad1 (.a(A[1]), .b(~B[1]), .cin(carry[0]), .sum(t_sum[1]), .cout(carry[1]));
 		FullAdder fad2 (.a(A[2]), .b(~B[2]), .cin(carry[1]), .sum(t_sum[2]), .cout(carry[2]));
-		FullAdder fad3 (.a(A[2]), .b(~B[2]), .cin(carry[2]), .sum(t_sum[3]), .cout(carry[3]));
-		FullAdder fad4 (.a(A[3]), .b(~B[3]), .cin(carry[3]), .sum(t_sum[4]), .cout(cout));
+		FullAdder fad3 (.a(A[3]), .b(~B[3]), .cin(carry[2]), .sum(t_sum[3]), .cout(carry[3]));
+		FullAdder fad4 (.a(A[4]), .b(~B[4]), .cin(carry[3]), .sum(t_sum[4]), .cout(cout));
 
 	   assign Sum = {cout,t_sum} + (`W-1);
 endmodule
